f2e881bb569d0e36b60352ceee1c0788afaafcfd
Ismael <ifiguera@almata.cat>
2025-01-31 13:08:09 +0100

    correu fet Ismael

--- database/migrations/2025_01_31_113212_create_password_resets_table.php
+++ database/migrations/2025_01_31_113212_create_password_resets_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->id();
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
             $table->timestamps();
         });
     }
@@ -22,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('password_reset_tokens');
     }
 };
